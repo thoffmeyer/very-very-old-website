8047879292bfbf00c0a6525738ae1b9c89d32bdf
Timothy Hoffmeyer <tim.hoffmeyer@gmail.com>
2009-05-04 03:15:51 -0400

    removed some stuff

--- cms/.in/head.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-<head>
-  <link rel="stylesheet" href=".in/style.css" type="text/css">
-  <link rel="icon" href=".im/favicon.ico" type="image/ico">
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
